fd2e3f125276e0b3210f244066339269db06a9b6
Daniel Dominguez <Daniel Dominguez>
2016-10-19 19:39:26 -0600

    Removing generated files

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
+const vulcanize = require('gulp-vulcanize');
 
 gulp.task('sass', function() {
 	return gulp.src('web/sass/*.scss')
@@ -19,11 +20,11 @@ gulp.task('sass', function() {
 });
 
 gulp.task('templates', function() {
-	return gulp.src('web/templates/**/*')
-		.pipe(gulp.dest('app/dist/components'))
+    return gulp.src('web/templates/src/nm-elements.html')
+        .pipe(vulcanize())
+        .pipe(gulp.dest('app/dist/components'));
 });
- 
- 
+
 gulp.task('js', function () {
     // app.js is your main JS file with all your module inclusions
     return browserify({entries: './web/js/app.js', debug: true})
@@ -34,5 +35,19 @@ gulp.task('js', function () {
         .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./app/dist/js'));
+        .pipe(gulp.dest('app/dist/js'));
 });
+
+gulp.task('copyJs', function () {
+    return gulp.src([
+        "./web/templates/bower_components/webcomponentsjs/webcomponents-lite.min.js"
+    ]).pipe(gulp.dest("app/dist/js"))
+});
+
+gulp.task('copyView', function () {
+    return gulp.src([
+        "./web/view/**"
+    ]).pipe(gulp.dest("app/view"))
+});
+
+gulp.task('default', ["templates", "js", "sass", "copyJs", "copyView"])
