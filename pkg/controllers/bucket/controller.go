6a9e3dbbb2fe864941f668773eee7d60a9275631
Daniel J Dominguez <thinking.danield@gmail.com>
2017-02-05 18:49:43 -0700

    Added Set to bucket

--- pkg/controllers/bucket/controller.go
+++ pkg/controllers/bucket/controller.go
@@ -3,15 +3,18 @@ package bucket
 import (
 	"golang.org/x/net/context"
 	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
 )
 
+const entity = "bucket"
+
 //Get gets an item from the bucket with the same field
-func Get(context context.Context, field string) (item Item, err error) {
+func Get(c context.Context, field string) (item Item, err error) {
 	var items []Item
 
-	q := datastore.NewQuery("Bucket").Filter("Field =", field).Limit(1)
+	q := datastore.NewQuery(entity).Filter("Field =", field).Limit(1)
 
-	_, err = q.GetAll(context, items)
+	_, err = q.GetAll(c, items)
 
 	if err != nil {
 		return
@@ -25,15 +28,33 @@ func Get(context context.Context, field string) (item Item, err error) {
 }
 
 //GetAll gets all items with the fields listed
-func GetAll(context context.Context, fields ...string) (items []Item, err error) {
+func GetAll(c context.Context, fields ...string) (items []Item, err error) {
 	for _, f := range fields {
 		var item Item
-		item, err = Get(context, f)
+		item, err = Get(c, f)
 		if err != nil {
 			return
 		}
 		items = append(items, item)
 	}
 
+	return
+}
+
+func Set(c context.Context, item Item) (err error) {
+	var key *datastore.Key
+	q := datastore.NewQuery(entity).Filter("Field =", item.Field).Limit(1)
+	keys, dErr := q.GetAll(c, &item)
+	if dErr != nil {
+		log.Warningf(c, "bucket.Set - Unable to look into bucket\n%v", dErr)
+	}
+
+	if dErr != nil || len(keys) == 0 {
+		key = datastore.NewIncompleteKey(c, entity, nil)
+	} else {
+		key = keys[0]
+	}
+
+	_, err = datastore.Put(c, key, &item)
 	return
 }
