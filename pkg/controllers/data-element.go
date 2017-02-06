deeb51d7804e01c1e418f908443791032c5cea03
Daniel J Dominguez <thinking.danield@gmail.com>
2017-02-05 19:03:01 -0700

    Removes UUID

--- pkg/controllers/data-element.go
+++ pkg/controllers/data-element.go
@@ -6,7 +6,6 @@ import (
 
 //DataElement contains basic informtion on data that has been retrieved by a database
 type DataElement struct {
-	UUID       string
 	CreatedOn  time.Time
 	CreatedBy  string
 	ModifiedOn time.Time
