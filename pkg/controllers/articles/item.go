28a1c6cdc4a8c487d6fb359233b857eb45e7ae58
Daniel J Dominguez <thinking.danield@gmail.com>
2017-02-11 21:29:07 -0700

    Now able to clean html

--- pkg/controllers/articles/item.go
+++ pkg/controllers/articles/item.go
@@ -1,6 +1,7 @@
 package articles
 
 import (
+	"log"
 	"strings"
 
 	"html/template"
@@ -11,12 +12,11 @@ import (
 //Article contains information about articles written on this website.
 type Article struct {
 	controllers.DataElement
-	Type            string
-	Key             string
-	Title           string
-	Abstract        string
-	MarkdownContent string
-	HTMLContent     template.HTML
+	Type        string
+	Key         string
+	Title       string
+	Abstract    string `datastore:"noindex"`
+	HTMLContent []byte `datastore:"noindex"`
 }
 
 //Path returns the path for a article.
@@ -27,4 +27,15 @@ func (a Article) Path() string {
 //Heading returns a heading for the article using the Type and Title.
 func (a Article) Heading() string {
 	return strings.Title(a.Type + ": " + a.Title)
+}
+
+//Content returns HTMLContent after it has been parsed and cleaned
+func (a Article) Content() (content template.HTML) {
+	var err error
+
+	content, err = controllers.CleanHTML(a.HTMLContent)
+	if err != nil {
+		log.Printf("article.Content - Unable to clean HTML\n%v", err)
+	}
+	return
 }
---
+++ pkg/controllers/clean-html.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	"bytes"
+	"html/template"
+
+	"github.com/microcosm-cc/bluemonday"
+	"golang.org/x/net/html"
+)
+
+func CleanHTML(dirtyHTML []byte) (cleanHTML template.HTML, err error) {
+	reader := bytes.NewReader(dirtyHTML)
+	htmlNodes, pErr := html.Parse(reader)
+	if pErr != nil {
+		err = pErr
+		return
+	}
+
+	var renderedHTML bytes.Buffer
+	html.Render(&renderedHTML, htmlNodes)
+
+	policy := bluemonday.NewPolicy()
+	policy.AllowElements("i", "b", "strong", "em", "a", "p")
+	policy.AllowAttrs("href").OnElements("a")
+	policy.RequireParseableURLs(true)
+	policy.AllowRelativeURLs(true)
+	policy.RequireNoFollowOnFullyQualifiedLinks(true)
+	cleanBytes := policy.SanitizeBytes(renderedHTML.Bytes())
+	cleanHTML = template.HTML(cleanBytes)
+	return
+}
