2503f2c1cf9e71e993fa6050da60c33f3cc5a70d
Daniel J Dominguez <thinking.danield@gmail.com>
2017-05-04 21:34:45 -0600

    Creating page to show all categories

--- server/controllers/admin/categories.go
+++ server/controllers/admin/categories.go
@@ -7,25 +7,24 @@ import (
 	"github.com/danield21/danield-space/server/controllers/status"
 	"github.com/danield21/danield-space/server/controllers/view"
 	"github.com/danield21/danield-space/server/handler"
-	"github.com/danield21/danield-space/server/repository/articles"
 	"github.com/danield21/danield-space/server/repository/categories"
 	"github.com/danield21/danield-space/server/repository/siteInfo"
 	"golang.org/x/net/context"
 )
 
-var IndexHeadersHandler = AdminHeadersHandler
+var CategoryHeadersHandler = AdminHeadersHandler
 
-var IndexPageHandler = handler.Chain(
+var CategoryPageHandler = handler.Chain(
 	view.HTMLHandler,
 	handler.ToLink(handler.Chain(
-		IndexHeadersHandler,
-		IndexPageLink,
+		CategoryHeadersHandler,
+		CategoryPageLink,
 		link.Theme,
 		status.LinkAll,
 	)),
 )
 
-func IndexPageLink(h handler.Handler) handler.Handler {
+func CategoryPageLink(h handler.Handler) handler.Handler {
 	return func(ctx context.Context, e handler.Environment, w http.ResponseWriter) (context.Context, error) {
 		ses := handler.Session(ctx)
 
@@ -36,12 +35,10 @@ func IndexPageLink(h handler.Handler) handler.Handler {
 
 		info := siteInfo.Get(ctx)
 		cats, _ := categories.GetAll(ctx)
-		arts, _ := articles.GetAll(ctx, 1)
 
 		data := struct {
 			AdminModel
-			HasCategories bool
-			HasArticles   bool
+			Categories []*categories.Category
 		}{
 			AdminModel: AdminModel{
 				BaseModel: view.BaseModel{
@@ -49,14 +46,9 @@ func IndexPageLink(h handler.Handler) handler.Handler {
 				},
 				User: user,
 			},
-			HasCategories: len(cats) > 0,
-			HasArticles:   len(arts) > 0,
+			Categories: cats,
 		}
 
-		return h(link.PageContext(ctx, "page/admin/index", data), e, w)
+		return h(link.PageContext(ctx, "page/admin/category", data), e, w)
 	}
 }
-
-var ArticlesHeadersHandler = AdminHeadersHandler
-
-var ArticlesPageHandler = NewAdminPageHandler("page/admin/article")
